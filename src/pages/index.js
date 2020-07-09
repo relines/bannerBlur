a1fe7f13598af8fc104ec6750922e7a2cf815f50
reline <reline@jhy.local>
2020-07-09 18:57:42 +0800

    class版本

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,6 @@ class Swiper extends Component {
     super(props);
     this.state = {
       currentIndex: 0,
-      bgShow: false
     };
     this.myList = [];
   }
@@ -49,13 +48,29 @@ class Swiper extends Component {
     this.myList.forEach((item, index) => {
       this.getBase64ByUrl(item.url).then(resp => {
         item.base64Url = resp;
-        // if (index === 0) {
-        //   this._handleChangeIndex(0);
-        // }
+        if (index === 0) {
+          this.handleChangeIndex(0);
+        }
       });
     });
-    // this.interval = setInterval(this._handleChangeIndex, 7000);
+    this.interval = setInterval(this.handleChangeIndex, 3000);
   }
+  componentWillUnmount() {
+    this.handleClearInterval();
+  }
+  imgLoaded = index => {
+    this.goBlur(this.refs["img" + index], this.refs["canvas" + index]);
+  };
+  goBlur = (imgDom, canvasDom) => {
+    StackBlur.image(imgDom, canvasDom, 20, true);
+    let ch = document.body.clientWidth;
+    canvasDom.style.width = "100%";
+    if (parseInt(ch) < 1353) {
+      canvasDom.style.height = "100%";
+    } else {
+      canvasDom.style.height = "auto";
+    }
+  };
   getBase64ByUrl = (src, outputFormat) => {
     return new Promise((resolve, reject) => {
       var xhr = new XMLHttpRequest();
@@ -71,7 +86,8 @@ class Swiper extends Component {
           }
           var data = binaryString.join("");
           var base64 = window.btoa(data);
-          var dataUrl = "data:" + (outputFormat || "image/png") + ";base64," + base64;
+          var dataUrl =
+            "data:" + (outputFormat || "image/png") + ";base64," + base64;
           resolve(dataUrl);
         }
       };
@@ -79,11 +95,44 @@ class Swiper extends Component {
     });
   };
 
+  handleMouse = val => {
+    switch (val) {
+      case "hover":
+        this.handleClearInterval();
+        break;
+      case "leave":
+        this.interval = setInterval(this.handleChangeIndex, 3000);
+        break;
+      default:
+    }
+  };
+
+  handleChangeIndex = index => {
+    const { list } = this.props;
+    this.setState(prevState => ({
+      currentIndex:
+        index === undefined
+          ? prevState.currentIndex + 1 >= list.length
+            ? 0
+            : prevState.currentIndex + 1
+          : index
+    }));
+  };
+
+  handleClearInterval() {
+    this.interval && clearInterval(this.interval);
+  }
+
+
   render() {
     const { list } = this.props;
-    const { currentIndex, bgShow } = this.state;
+    const { currentIndex } = this.state;
     return (
-      <div className={styles.swiper}>
+      <div
+        className={styles.swiper}
+        onMouseEnter={() => this.handleMouse("hover")}
+        onMouseLeave={() => this.handleMouse("leave")}
+      >
         <div className={styles.swiperContainer}>
           {
             this.myList.map((item, index) => {
@@ -94,12 +143,42 @@ class Swiper extends Component {
                   })}
                   key={index}
                 >
-                  <img className={styles.imgBehind} src={item.url} alt="img elements must have an alt props" />
+                  <img
+                    className={styles.imgBehind}
+                    src={item.url}
+                    ref={"img" + index}
+                    alt="img elements must have an alt props"
+                  />
+                  <img
+                    className={styles.imgAhead}
+                    src={item.url}
+                    onLoad={() => {
+                      this.imgLoaded(index);
+                    }}
+                    alt="img elements must have an alt props"
+                  />
+                  <canvas
+                    ref={"canvas" + index}
+                    className={styles.canvasBg}
+                  ></canvas>
                 </div>
               )
             })
           }
         </div>
+        <ul className={styles.switchList}>
+          {list &&
+            list.length > 1 &&
+            list.map((item, index) => {
+              return (
+                <li
+                  className={classnames({ [styles.cur]: currentIndex === index })}
+                  key={index}
+                  onClick={() => this.handleChangeIndex(index)}
+                />
+              );
+            })}
+        </ul>
       </div>
     );
   }
